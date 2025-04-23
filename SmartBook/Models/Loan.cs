1767c991dca7c8d6b7c742f33c8e126e4c5bdd34
1256ID <112385041+1256ID@users.noreply.github.com>
2025-04-23 11:19:12 +0200

    Adding relevant Models/Handlers.

---
+++ SmartBook/Models/Loan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBook.Models;
+ class Loan
+{
+    private int Id;   
+    private Guid ISBN;
+    private Guid UserId;
+    private DateTime LoanDate;
+    private DateTime DueDate;
+    private DateTime ReturnDate;
+
+    public Loan(int id, Guid iSBN, Guid userId)
+    {
+        Id = id;
+        ISBN = iSBN;
+        UserId = userId;
+    }
+}
---
+++ SmartBook/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBook.Models
+{
+    class User
+    {
+        public required Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
